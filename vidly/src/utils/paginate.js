d7d9a50c4370c602b9b239493f6a5ac6706dc9eb
mahesht10 <mahesh@autonom8.com>
2020-04-05 18:29:25 +0530

    fix: error rendering pagination data

--- vidly/src/utils/paginate.js
+++ vidly/src/utils/paginate.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
 export default function paginate(items, pageSize, currentPage) {
-  const startIndex = (pageSize - 1) * currentPage;
+  const startIndex = (currentPage - 1) * pageSize;
   return _(items).slice(startIndex).take(pageSize).value();
 }
